cf60fdd5e0e4446086cd6dadf239fffbfab29aff
Hlonela <82158647+Hlonela@users.noreply.github.com>
2021-04-25 00:31:26 +0200

    first commit

---
+++ Palidrome-odd/main.py
@@ -0,0 +1,27 @@
+                #TASK ONE:
+
+#input_numbers = input ("Please enter your integers: ") #Enter the integer
+#reserved = input_numbers [::-1] # reverse it
+
+#def reversed_version (*args):
+    #if input_numbers == reserved: #If the reversed form is the same as the input
+
+        #return True
+    #else:
+        #return False
+#print(reversed_version()) #I call the function to return True or False
+
+            #TASK TWO:
+
+enter_numbers = int (input("Please enter an integer of your choice: "))
+divided = enter_numbers % 2
+
+if divided == 0:
+    print("Even Number!")
+else:
+    print("Odd Number!")
+
+
+    print(divided)
+
+
